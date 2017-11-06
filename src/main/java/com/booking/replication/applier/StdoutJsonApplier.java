a9311981e7ab7ab10844ca82ab5c551cfbf13209
Quan Do <quan.dm@teko.vn>
2017-11-06 06:31:45 +0000

    update

--- src/main/java/com/booking/replication/applier/StdoutJsonApplier.java
+++ src/main/java/com/booking/replication/applier/StdoutJsonApplier.java
@@ -28,7 +28,7 @@ public class StdoutJsonApplier implements Applier  {
     // TODO: move these to Cmd config params
     public static final String FILTERED_TABLE_NAME = null;
     public static final Boolean VERBOSE = false;
-    public static final Boolean STATS_OUT = true;
+    public static final Boolean STATS_OUT = false;
     public static final Boolean DATA_OUT = false;
     public static final Boolean SCHEMA_OUT = false;
 
